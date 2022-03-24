a5a694a319dfe9b6fb0b2878586e6fa678cd4d77
sofiamontebugnoli <67460826+sofiamontebugnoli@users.noreply.github.com>
2022-03-24 12:08:25 +0100

    creation of WordleTester, bug fixes at WordleAISofia and WordleList, creation of tester notebook for inspecting most popular letters

--- WordList.py
+++ WordList.py
@@ -1,12 +1,16 @@
-import string
+from typing import List
+"""
 import requests
 import re
+"""
+import string
 
 class WordList:
     """
     Reads a list of words from a file and filters it for wordle compatible words.
     Used to provide word list copies.
     """
+    words: List[str]
 
     def __init__(self, filename):
         self.ascii_lowercase = list(string.ascii_lowercase)
@@ -15,14 +19,14 @@ class WordList:
             self.words = [word.rstrip() for word in self.words]
             self.words = [w for w in self.words if len(w) == 5 and self.is_ascii_lowercase(w)]
             self.words = list(dict.fromkeys(self.words))  # remove duplicates
-
+    """
     def __init__(self):
         self.ascii_lowercase = list(string.ascii_lowercase)
 
         url = 'https://www-cs-faculty.stanford.edu/~knuth/sgb-words.txt'
         words_site = requests.get(url)
 
-        with open('data/common_words_5_letters.txt', 'wb')as file:
+        with open('data/common_words_5_letters.txt', 'wb') as file:
             file.write(words_site.content)
 
         with open('data/common_words_5_letters.txt', encoding="utf8") as file:
@@ -30,6 +34,7 @@ class WordList:
             self.words = [word.rstrip() for word in self.words]
             self.words = [w for w in self.words if len(w) == 5 and self.is_ascii_lowercase(w)]
             self.words = list(dict.fromkeys(self.words))  # remove duplicates
+    """
 
     def get_list_copy(self):
         return self.words.copy()
@@ -40,29 +45,30 @@ class WordList:
                 return False
         return True
 
-    #da la popolarità della lettera
+"""
+    # da la popolarità della lettera cercando in ogni parola del dizionario conta la popolarità delle lettere
     def get_dict_letters_percentages(self):
         all_letters = string.ascii_lowercase[:]
 
         letter_stats_dict = {}
         for letter in all_letters:
-            letter_stats_dict[letter]=0
+            letter_stats_dict[letter] = 0
 
-        total_letters = len(self.words)*5.0
+        total_letters = len(self.words) * 5.0
 
         for letter in all_letters:
             for word in self.words:
-                letter_stats_dict[letter]+=word.count(letter)
+                letter_stats_dict[letter] += word.count(letter)
 
         total = 0
         for letter in all_letters:
-            letter_stats_dict[letter]/=total_letters
+            letter_stats_dict[letter] /= total_letters
             total = total + letter_stats_dict[letter]
 
         return letter_stats_dict
 
-    #da il best score non sapendo la popolarità delle parole usate
-    #si basa solo sulla popolarità delle lettere che compongono la parola
+    # da il best score non sapendo la popolarità delle parole usate
+    # si basa solo sulla popolarità delle lettere che compongono la parola
     def get_best_score_dict(self):
         letter_stats_dict = self.get_dict_letters_percentages()
         word_stat_dict = {}
@@ -72,13 +78,11 @@ class WordList:
         for word in self.words:
             unq_char_word = "".join(set(word))
             for letter in unq_char_word:
-                word_stat_dict[word]+=letter_stats_dict[letter]
+                word_stat_dict[word] += letter_stats_dict[letter]
 
-
-        word_stat_dict = sorted(word_stat_dict.items(), key=lambda x:x[1],reverse=True)
+        word_stat_dict = sorted(word_stat_dict.items(), key=lambda x: x[1], reverse=True)
         word_stat_dict = dict(word_stat_dict)
         return word_stat_dict
-
 
     def get_next_best_guesses(self, first_word):
         string_regex = '^[^' + first_word + ']+$'
@@ -87,3 +91,4 @@ class WordList:
         for word, information_value in self.get_best_score_dict().items():
             if re.search(regex, word):
                 return word
+"""
---
+++ WordProbability.py
@@ -0,0 +1,27 @@
+from WordList import WordList
+
+
+class WordProbability:
+    """
+    Helper class to take into account how common words are in the English language.
+    """
+
+    def __init__(self):
+        self.common_words = WordList("data/common_words.txt").words
+        self.probability = {}
+        self.words = WordList("data/combined_wordlist.txt").words
+        for word in self.words:
+            self.probability[word] = self._calculate_probability(word)
+
+    def _calculate_probability(self, word):
+        if word not in self.common_words:
+            return 368 / (368 + 8890)
+        relative_position = self.common_words.index(word) / len(self.common_words)
+        return 0.85 * (1 - relative_position) + 0.35 * relative_position
+
+    def is_wordle_probability(self, word):
+        """
+        :param word: a 5 letter word
+        :return: the probability of the word being a wordle based on its popularity in the English language
+        """
+        return self.probability[word]
